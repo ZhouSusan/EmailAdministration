6f18b5c195abf928a0615cf6ac45815bc0deb809
ZhouSusan <szhou089@gmail.com>
2022-11-03 15:55:37 -0700

    added to variables to Class Email in Email.java

---
+++ src/main/java/org/example/Email.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String secondaryEmail;
+}
