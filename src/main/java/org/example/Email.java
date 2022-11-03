c780e2c78bb9c20f49112821b972e6ff463e49b1
ZhouSusan <szhou089@gmail.com>
2022-11-03 16:40:14 -0700

    imported Scanner package and started setDepartment() method in Email.java

--- src/main/java/org/example/Email.java
+++ src/main/java/org/example/Email.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.util.Scanner;
 
 public class Email {
     private String firstName;
@@ -7,11 +8,32 @@ public class Email {
     private String department;
     private int mailBoxCapacity;
     private String secondaryEmail;
-    
+
     public Email(String firstname, String lastName) {
         this.firstName = firstname;
         this.lastName = lastName;
         System.out.println("Email Created for : " + lastName + ", " + firstname );
+    }
+
+    private String setDepartment() {
+        System.out.print("Please select your department: Enter\n 1 - Sales\n 2 - Development\n 3- Accounting\n");
+        Scanner in = new Scanner(System.in);
+        int choice = in.nextInt();
+        switch (choice) {
+            case 1:
+                 System.out.println("You have selected Sales department.");
+                break;
+            case 2:
+                System.out.println("You have selected Development Department.");
+                break;
+            case 3:
+                System.out.println("You have selected Accounting department.");
+                break;
+            default:
+                System.out.println("You have selected no department.");
+                break;
+        }
+
     }
 
 }
